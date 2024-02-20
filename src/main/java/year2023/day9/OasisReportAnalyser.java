9b17d1db795ae2b16f050759321ca89436a31a1c
weiting-1993 <w.lin@rebuy.com>
2024-02-20 23:47:25 +0100

    feat: solution for day 9 part 2

--- src/main/java/year2023/day9/OasisReportAnalyser.java
+++ src/main/java/year2023/day9/OasisReportAnalyser.java
@@ -13,26 +13,33 @@ public class OasisReportAnalyser
 {
     private static final String INPUT_FILE_PATH = "src/main/java/year2023/day9/input.txt";
     private static final Logger logger = Logger.getLogger(OasisReportAnalyser.class.getName());
+    private static final String NEXT_RECORD = "next";
+    private static final String PREV_RECORD = "previous";
+
 
     public static void main(String[] args)
     {
         try {
             List<String> input = Files.readAllLines(Paths.get(INPUT_FILE_PATH));
-            List<Integer> extrapolatedValueOfEachHistory = new ArrayList<>();
+            List<Integer> nextExtrapolatedValueOfEachHistory = new ArrayList<>();
+            List<Integer> previousExtrapolatedValueOfEachHistory = new ArrayList<>();
 
             for (String line: input) {
                 List<Integer> historicalData = Arrays.stream(line.trim().split(" ")).map(Integer::parseInt).toList();
-                int extrapolatedValue = calculateExtrapolatedValue(historicalData);
-                extrapolatedValueOfEachHistory.add(extrapolatedValue);
+                int nextExtrapolatedValue = calculateExtrapolatedValue(historicalData, NEXT_RECORD);
+                int prevExtrapolatedValue = calculateExtrapolatedValue(historicalData, PREV_RECORD);
+                nextExtrapolatedValueOfEachHistory.add(nextExtrapolatedValue);
+                previousExtrapolatedValueOfEachHistory.add(prevExtrapolatedValue);
             }
 
-            System.out.println("Part 1: " + extrapolatedValueOfEachHistory.stream().mapToInt(Integer::intValue).sum());
+            System.out.println("Part 1: " + nextExtrapolatedValueOfEachHistory.stream().mapToInt(Integer::intValue).sum());
+            System.out.println("Part 2: " + previousExtrapolatedValueOfEachHistory.stream().mapToInt(Integer::intValue).sum());
         } catch (IOException e) {
             logger.log(Level.WARNING, "An error occurred while trying to read a file.", e);
         }
     }
 
-    public static int calculateExtrapolatedValue(List<Integer> historicalData)
+    public static int calculateExtrapolatedValue(List<Integer> historicalData, String when)
     {
         List<List<Integer>> differenceListCollection = new ArrayList<>();
         boolean finishCalculatingTheDifference = false;
@@ -48,14 +55,18 @@ public class OasisReportAnalyser
             }
         }
 
-        int extrapolatedValue = historicalData.getLast();
+        int extrapolatedValue = when.equals(NEXT_RECORD) ? historicalData.getLast() : historicalData.getFirst();
         int accumulatedValue = 0;
 
         for (int i = differenceListCollection.size() - 1; i >= 0; i--) {
-            accumulatedValue += differenceListCollection.get(i).getLast();
+            if (when.equals(NEXT_RECORD)) {
+                accumulatedValue += differenceListCollection.get(i).getLast();
+            } else {
+                accumulatedValue = differenceListCollection.get(i).getFirst() - accumulatedValue;
+            }
         }
 
-        return extrapolatedValue + accumulatedValue;
+        return when.equals(NEXT_RECORD) ? extrapolatedValue + accumulatedValue : extrapolatedValue - accumulatedValue;
     }
 
     public static List<Integer> calculateDifferences(List<Integer> originalList) {
